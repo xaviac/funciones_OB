4b149a263fa6cf510bbd99e5293ee85708b30aa7
xaviac <jacevedomx@gmail.com>
2022-04-07 23:34:48 -0500

    Creación de la suma con tres parámetros

---
+++ src/OpenB/Main.java
@@ -0,0 +1,14 @@
+package OpenB;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+       // suma(5, 9, 7);
+        System.out.println(suma(5,9,7));
+    }
+
+    public static int suma(int a, int b, int c){
+        return a + b + c;
+    }
+}
